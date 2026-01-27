9d1abfb6246eb9222483780a0d86c21e33d56253
rakeshbabu18 <gubbala2005@gmail.com>
2026-01-27 23:38:03 +0530

    week-2 assignment

---
+++ week-2/master-project-on-collections/roles.js
@@ -0,0 +1,21 @@
+const roles = {
+  admin: ["create", "update", "delete", "view"],
+  student: ["view"]
+};
+
+// Get all role names
+const roleNames = Object.keys(roles);
+
+// Check if student can delete
+const studentCanDelete = roles.student.includes("delete");
+
+// Flat list of all unique permissions
+const allPermissions = [
+  ...new Set(Object.values(roles).flat())
+];
+
+// Add moderator role immutably
+const updatedRoles = {
+  ...roles,
+  moderator: ["view", "update"]
+};
